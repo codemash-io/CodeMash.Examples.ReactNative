c155c331d4cacf3598e4d081fe00b69b6e337aee
Domantas <domantasjovaisas@gmail.com>
2020-09-14 11:17:51 +0300

    useFind hook with more comments

--- List/src/config/ProjectConfig.js
+++ List/src/config/ProjectConfig.js
@@ -1,8 +1,8 @@
 export const PROJECT_CONFIG = {
-  LOADING_INDICATOR: {
-    NO: 0, // do not show loading indicator.
-    INITIAL_LOAD: 1, // load data for the frost time. 
-    PAGING: 2 // Load extra dara using paging.
+  DATA_LOADING: {
+    DO_NOT_LOAD: 0, // do not show loading indicator.
+    FIRST_TIME_LOAD: 1, // load data for the first time. 
+    EXTRA_ROUNDTRIP: 2 // Load extra dara using paging.
   },
   DEFAULT_PAGE_SIZE: 20,
 };
--- List/src/hooks/useFind.js
+++ List/src/hooks/useFind.js
@@ -5,7 +5,7 @@ import { PROJECT_CONFIG } from '../config/ProjectConfig';
 
 export default ({ collectionName, filter, pageSize, pageNumber, sort, projection, referencedFields }) => {
   const [result, setResult] = useState([]);
-  const [isLoading, setIsLoading] = useState(PROJECT_CONFIG.LOADING_INDICATOR.NO); 
+  const [isLoading, setIsLoading] = useState(PROJECT_CONFIG.DATA_LOADING.DO_NOT_LOAD); 
   const [isRefreshing, setIsRefreshing] = useState(false);
   const [errorMessage, setErrorMessage] = useState('');
   const [noMoreResults, setNoMoreResults] = useState(false);
@@ -14,17 +14,24 @@ export default ({ collectionName, filter, pageSize, pageNumber, sort, projection
     collectionName, filter, pageSize, pageNumber: pageNumber || 0, sort, projection, referencedFields,
   });
 
-  const find = async (refresh) => {
-    if ((refresh && isRefreshing) || (!refresh && (noMoreResults || isLoading !== 0))) return;
+  const find = async (loadExtraData) => {
+    // if we call find function for an extra data and it's still refreshing - return
+    // or if we call find for the first time return when 
+    //    no more data
+    //    already loading data (initial load | extra load)
+    if ((loadExtraData && isRefreshing) || (!loadExtraData && (noMoreResults || isLoading !== PROJECT_CONFIG.DATA_LOADING.DO_NOT_LOAD))) 
+      return;
     try {
-      if (refresh) setIsRefreshing(true);
-      else setIsLoading(totalGot === 0 ? 1 : 2);
-
-      console.log(requestData)
+      if (loadExtraData) 
+        setIsRefreshing(true);
+      else 
+        setIsLoading(totalGot === 0 
+          ? PROJECT_CONFIG.DATA_LOADING.FIRST_TIME_LOAD 
+          : PROJECT_CONFIG.DATA_LOADING.EXTRA_ROUNDTRIP);
 
       const response = await db.getRecords({
           collectionName: requestData.collectionName,
-          pageNumber: refresh ? 0 : requestData.pageNumber,
+          pageNumber: loadExtraData ? 0 : requestData.pageNumber,
           pageSize: requestData.pageSize || PROJECT_CONFIG.DEFAULT_PAGE_SIZE,
           sort: requestData.sort,
           filter: requestData.filter,
@@ -37,32 +44,40 @@ export default ({ collectionName, filter, pageSize, pageNumber, sort, projection
       if (response && response.result) {
         let newTotalGot = 0;
 
-        if (refresh) {
+        // apply new result to existing data and set new total value
+        if (loadExtraData) {
           setResult([...response.result]);
 
           newTotalGot = response.result.length;
           setTotalGot(newTotalGot);
-        } else {
+        } 
+        // set result for the first time and set total
+        else {
           setResult([...result, ...response.result]);
 
           newTotalGot = totalGot + response.result.length;
           setTotalGot(newTotalGot);
         }
 
+        // when all data is gathered set indicator noMoreResults as true. 
         if (newTotalGot >= response.totalCount) {
           setNoMoreResults(true);
-        } else {
+        } 
+        // otherwise allow extra roundtrips to the server
+        else {
           setNoMoreResults(false);
         }
       }
-
       // setNextPage(nextPage + 1);
-      setRequestData((prevData) => ({ ...prevData, pageNumber: refresh ? 1 : prevData.pageNumber + 1 }));
+      setRequestData((prevData) => ({ ...prevData, pageNumber: loadExtraData ? 1 : prevData.pageNumber + 1 }));
     } catch (e) {      
       setErrorMessage('Something went wrong');
     } finally {
-      if (refresh) setIsRefreshing(false);
-      else setIsLoading(0);
+      // disable refreshing and loading indicators.
+      if (loadExtraData) 
+        setIsRefreshing(false);
+      else 
+        setIsLoading(PROJECT_CONFIG.DATA_LOADING.DO_NOT_LOAD);
     }
   };
 
